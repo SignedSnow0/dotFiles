bbcbb8199628ce49a55ca76a57a389ba6f089b57
SignedSnow0 <marchiniclaudio03@gmail.com>
2026-05-07 09:48:42 +0200

    minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,12 @@ set fish_greeting
 
 export EDITOR=/usr/bin/nvim
 export LLVM_SYS_181_PREFIX=/lib/llvm18/
+export ANDROID_HOME=/opt/android-sdk
 
 starship init fish | source
 
 fish_add_path /home/signedsnow0/.spicetify
+fish_add_path /home/signedsnow0/.flutter/flutter/bin
+fish_add_path $ANDROID_HOME/tools
+fish_add_path $ANDROID_HOME/platform-tools
+fish_add_path $ANDROID_HOME/cmdline-tools/latest/bin
